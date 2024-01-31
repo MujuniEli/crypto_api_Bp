b46955f773708c0ec9b8c3ab320f115430513caf
MujuniEli <elijahmujuni@gmail.com>
2024-01-31 19:43:31 +0300

    added main section mark up

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@
 import React from 'react'
 import { Switch, Route, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
-import { Navbar } from './components'
+
+import { Navbar, Homepage, Exchanges, Cryptocurrencies, CryptoDetails } from './components'
 
 import './App.css';
 
@@ -13,7 +14,24 @@ const App = () => {
             <Navbar />
         </div>
         <div className='main'>
-
+            <Layout>
+              <div className='routes'>
+                <Switch>
+                  <Route exact path='/'>
+                    <Homepage />
+                  </Route>
+                  <Route exact path='/'>
+                    <Exchanges />
+                  </Route>
+                  <Route exact path='/'>
+                    <Cryptocurrencies />
+                  </Route>
+                  <Route exact path='/'>
+                    <CryptoDetails />
+                  </Route>
+                </Switch>
+              </div>
+            </Layout>
         </div>
         <div className='footer'>
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
 import icon from '../images/cryptocurrency.png'
 
-const navbar = () => {
+const Navbar = () => {
   return (
     <div className='nav-container'>
         <div className='logo-container'>
@@ -32,4 +32,4 @@ const navbar = () => {
   )
 }
 
-export default navbar
+export default Navbar;
