d7b970eb08eb53e823f6e71f239dd6b08f0196f8
MujuniEli <elijahmujuni@gmail.com>
2024-02-03 20:50:08 +0300

    Added components to App

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import  ReactDOM  from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
+import 'antd/dist/antd.css';
 
 ReactDOM.render(
         <Router>
