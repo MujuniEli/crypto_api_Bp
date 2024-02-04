4519901fc7cfc86ee3cdece529a232826a51618b
MujuniEli <elijahmujuni@gmail.com>
2024-02-04 20:18:55 +0300

    Added redux store

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,18 @@
+
+// const options = {
+//     method: 'GET',
+//     url: 'https://coinranking1.p.rapidapi.com/coins',
+//     params: {
+//       referenceCurrencyUuid: 'yhjMzLPhuIDl',
+//       timePeriod: '24h',
+//       'tiers[0]': '1',
+//       orderBy: 'marketCap',
+//       orderDirection: 'desc',
+//       limit: '50',
+//       offset: '0'
+//     },
+//     headers: {
+//       'X-RapidAPI-Key': 'a8649fc10cmsh35b45dbe1490226p13ed08jsn5fe8f10f8667',
+//       'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+//     }
+//   };
